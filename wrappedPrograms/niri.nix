8d43a51ec58ddb22ccca1d8d6f9a87e8b9a38868
skramsa <skramsa@gmail.com>
2026-04-27 19:57:58 +0000

    noctalia settings

--- wrappedPrograms/niri.nix
+++ wrappedPrograms/niri.nix
@@ -18,10 +18,10 @@
         # startNoctaliaExe = lib.getExe self.packages.${config.pkgs.stdenv.hostPlatform.system}.start-noctalia-shell;
         noctaliaExe = lib.getExe self.packages.${config.pkgs.stdenv.hostPlatform.system}.noctalia-shell;
       in {
-        prefer-no-csd = null;
+        prefer-no-csd = {};
 
         input = {
-          focus-follows-mouse = null;
+          focus-follows-mouse = {};
 
           keyboard = {
             xkb = {
@@ -32,8 +32,8 @@
           };
 
           touchpad = {
-            natural-scroll = null;
-            tap = null;
+            natural-scroll = {};
+            tap = {};
           };
 
           mouse = {
@@ -44,26 +44,26 @@
         binds = {
           "Mod+Return".spawn = config.terminal;
 
-          "Mod+Q".close-window = null;
-          "Mod+F".maximize-column = null;
-          "Mod+G".fullscreen-window = null;
-          "Mod+Shift+F".toggle-window-floating = null;
-          "Mod+C".center-column = null;
+          "Mod+Q".close-window = {};
+          "Mod+F".maximize-column = {};
+          "Mod+G".fullscreen-window = {};
+          "Mod+Shift+F".toggle-window-floating = {};
+          "Mod+C".center-column = {};
 
-          "Mod+H".focus-column-left = null;
-          "Mod+L".focus-column-right = null;
-          "Mod+K".focus-window-up = null;
-          "Mod+J".focus-window-down = null;
+          "Mod+H".focus-column-left = {};
+          "Mod+L".focus-column-right = {};
+          "Mod+K".focus-window-up = {};
+          "Mod+J".focus-window-down = {};
 
-          "Mod+Left".focus-column-left = null;
-          "Mod+Right".focus-column-right = null;
-          "Mod+Up".focus-window-up = null;
-          "Mod+Down".focus-window-down = null;
+          "Mod+Left".focus-column-left = {};
+          "Mod+Right".focus-column-right = {};
+          "Mod+Up".focus-window-up = {};
+          "Mod+Down".focus-window-down = {};
 
-          "Mod+Shift+H".move-column-left = null;
-          "Mod+Shift+L".move-column-right = null;
-          "Mod+Shift+K".move-window-up = null;
-          "Mod+Shift+J".move-window-down = null;
+          "Mod+Shift+H".move-column-left = {};
+          "Mod+Shift+L".move-column-right = {};
+          "Mod+Shift+K".move-window-up = {};
+          "Mod+Shift+J".move-window-down = {};
 
           "Mod+1".focus-workspace = "w0";
           "Mod+2".focus-workspace = "w1";
@@ -98,10 +98,10 @@
           "Mod+Ctrl+J".set-window-height = "-5%";
           "Mod+Ctrl+K".set-window-height = "+5%";
 
-          "Mod+WheelScrollDown".focus-column-left = null;
-          "Mod+WheelScrollUp".focus-column-right = null;
-          "Mod+Ctrl+WheelScrollDown".focus-workspace-down = null;
-          "Mod+Ctrl+WheelScrollUp".focus-workspace-up = null;
+          "Mod+WheelScrollDown".focus-column-left = {};
+          "Mod+WheelScrollUp".focus-column-right = {};
+          "Mod+Ctrl+WheelScrollDown".focus-workspace-dMicrophoneown = {};
+          "Mod+Ctrl+WheelScrollUp".focus-workspace-up = {};
 
           "Mod+Ctrl+S".spawn-sh = ''${lib.getExe config.pkgs.grim} -l 0 - | ${config.pkgs.wl-clipboard}/bin/wl-copy'';
 
@@ -124,22 +124,12 @@
             {
               key = "w";
               desc = "Wifi";
-              cmd = "${noctaliaExe} ipc call wifi togglePanel";
+              cmd = "${noctaliaExe} ipc call network togglePanel";
             }
             {
               key = "f";
               desc = "Firefox";
               cmd = "firefox";
-            }
-            {
-              key = "t";
-              desc = "Telegram";
-              cmd = "Telegram";
-            }
-            {
-              key = "d";
-              desc = "Discord";
-              cmd = "vesktop";
             }
             {
               key = "m";
